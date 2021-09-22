a7efb270c34854450dd199d8b49c802a9923ae31
Demidovich Daniil <demidovich.daniil@gmail.com>
2021-09-22 22:16:46 +0300

    chore: update api path for import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,7 @@
-const BASE_PATH = 'https://oxgr1oja31.execute-api.eu-west-1.amazonaws.com/dev';
-
 const API_PATHS = {
-  product: `${BASE_PATH}/products`,
+  product: 'https://oxgr1oja31.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://y99qpj552a.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
