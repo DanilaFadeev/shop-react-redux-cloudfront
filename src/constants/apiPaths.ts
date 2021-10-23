40f206179e43c1af0e28145f28cac89fdc17beb6
Demidovich Daniil <demidovich.daniil@gmail.com>
2021-10-23 17:27:01 +0300

    feat: attach cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@ const API_PATHS = {
   product: 'https://oxgr1oja31.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://y99qpj552a.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://oxgr1oja31.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://danilafadeev-cart-api-development.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
